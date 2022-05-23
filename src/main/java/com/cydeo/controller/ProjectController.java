eb4571f05396d07b91a3d53541266e84fc6e694b
Veronica <zeballos.veronica@gmail.com>
2022-05-22 20:46:03 -0400

    project create

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -6,6 +6,7 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -29,5 +30,13 @@ public class ProjectController {
         model.addAttribute("managers",userService.findAll());
 
         return "/project/create";
+    }
+
+    @PostMapping("/create")
+    public String insertProject(ProjectDTO project){
+
+        projectService.save(project);
+
+        return "redirect:/project/create";
     }
 }
