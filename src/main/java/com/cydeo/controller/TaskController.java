a81a2d83a1f2b3f20bbe256e6f271225ccd91400
Veronica <zeballos.veronica@gmail.com>
2022-05-27 22:37:39 -0400

    task update #2

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -63,10 +63,18 @@ public class TaskController {
         return "/task/update";
     }
 
-    @PostMapping("/update/{taskId}")
-    public String updateTask(@PathVariable("taskId") Long taskId, TaskDTO task){
+//    @PostMapping("/update/{taskId}")
+//    public String updateTask(@PathVariable("taskId") Long taskId, TaskDTO task){
+//
+//        task.setId(taskId);
+//        taskService.update(task);
+//
+//        return "redirect:/task/create";
+//    }
 
-        task.setId(taskId);
+    @PostMapping("/update/{id}")
+    public String updateTask(TaskDTO task){
+
         taskService.update(task);
 
         return "redirect:/task/create";
